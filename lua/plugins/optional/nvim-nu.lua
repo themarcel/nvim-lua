5c1aaa1f7fe6cd5af73bc5a4556ef04b5f07c987
marcelarie <googlillo@gmail.com>
2026-04-02 17:53:47 +0200

    update

--- lua/plugins/optional/nvim-nu.lua
+++ lua/plugins/optional/nvim-nu.lua
@@ -18,11 +18,7 @@ return {
 	end,
 	dependencies = {
 		{
-			"nushell/tree-sitter-nu",
 			"jose-elias-alvarez/null-ls.nvim",
-			-- dir = "~/clones/forks/tree-sitter-nu",
-			-- build = ":TSUpdate nu",
-			-- commit = "e8bdcb9d4d57d999ad6d7c797f9f1808623df1b9",
 		},
 	},
 }
--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -10,38 +10,40 @@ end
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
+	-- We skip nvim-treesitter.configs entirely as it is removed in v1.0+
+	
 	safe_require("nvim-treesitter.parsers", function(parsers)
+		-- Attempt to find the parser configuration table safely
 		local parser_config
-		if type(parsers.get_parser_configs) == "table" then
+		if type(parsers.get_parser_configs) == "function" then
+			parser_config = parsers.get_parser_configs()
+		elseif type(parsers.get_parser_configs) == "table" then
 			parser_config = parsers.get_parser_configs
 		else
-			parser_config = parsers.get_parser_configs()
+			-- Fallback for the newest v1.0+ structure
+			parser_config = parsers
 		end
 
-		parser_config.kanshi = {
-			install_info = {
-				url = "https://github.com/marcelarie/tree-sitter-kanshi",
-				-- url = "~/clones/own/tree-sitter-kanshi",
-				files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-				-- optional entries:
-				branch = "main", -- default branch in case of git repo if different from master
-				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-			},
-			filetype = "kanshi", -- if filetype does not match the parser name
-		}
+		-- Check if we actually got a table we can write to
+		if type(parser_config) == "table" then
+			parser_config.kanshi = {
+				install_info = {
+					url = "https://github.com/marcelarie/tree-sitter-kanshi",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "kanshi",
+			}
 
-		parser_config.zig = {
-			install_info = {
-				url = "~/clones/forks/tree-sitter-zig",
-				files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-				-- optional entries:
-				branch = "main", -- default branch in case of git repo if different from master
-				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-			},
-			filetype = "zig", -- if filetype does not match the parser name
-		}
+			parser_config.zig = {
+				install_info = {
+					url = "~/clones/forks/tree-sitter-zig",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "zig",
+			}
+		end
 	end)
 end, 0)
 

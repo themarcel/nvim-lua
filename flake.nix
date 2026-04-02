38b98ff6ae83275a8203311686d6a27318ce7780
marcelarie <googlillo@gmail.com>
2026-04-02 17:16:12 +0200

    update config

--- flake.nix
+++ flake.nix
@@ -5,111 +5,112 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    neovimTools = pkgs:
+      with pkgs; [
+        # Core
+        luajit
+        lua54Packages.luarocks-nix
+        lua51Packages.lua
+        lua-language-server
+        luarocks
+        readline
+        fzf
+        fd
+        ripgrep
+        bat
+        tree-sitter
+        gcc
+        python3Minimal
+
+        # VSCode Language Servers (JSON, CSS, HTML, ESLint)
+        vscode-langservers-extracted
+
+        # LSP Servers
+        astro-language-server
+        bash-language-server
+        clang-tools
+        elixir-ls
+        fennel-ls
+        marksman
+        markdown-oxide
+        nil
+        python3Packages.python-lsp-server
+        rust-analyzer
+        taplo
+        typescript-language-server
+        vale-ls
+        mdx-language-server
+
+        # Formatters
+        alejandra
+        biome
+        python3Packages.black
+        cbfmt
+        deno
+        dprint
+        eslint_d
+        fixjson
+        python3Packages.isort
+        oxlint
+        prettier
+        prettierd
+        rustfmt
+        shfmt
+        stylua
+        # tombi not available in nixpkgs
+        xmlformat
+
+        # Debuggers
+        delve
+        vscode-extensions.vadimcn.vscode-lldb
+
+        # CLI Tools
+        lazygit
+        tig
+        gcc
+        gnumake
+        cargo
+        direnv
+        nodejs
+        yarn
+        docker
+        difftastic
+        util-linux
+        coreutils
+        curl
+        bash
+        findutils
+        zoxide
+      ];
+  in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    let
-      neovimTools =
-        pkgs: with pkgs; [
-          # Core
-          luajit
-          lua54Packages.luarocks-nix
-          lua51Packages.lua
-          lua-language-server
-          luarocks
-          readline
-          fzf
-          fd
-          ripgrep
-          bat
-          tree-sitter
-          gcc
-          python3Minimal
-
-          # VSCode Language Servers (JSON, CSS, HTML, ESLint)
-          vscode-langservers-extracted
-
-          # LSP Servers
-          astro-language-server
-          bash-language-server
-          clang-tools
-          elixir-ls
-          fennel-ls
-          marksman
-          markdown-oxide
-          nil
-          python3Packages.python-lsp-server
-          rust-analyzer
-          taplo
-          typescript-language-server
-          vale-ls
-          mdx-language-server
-
-          # Formatters
-          alejandra
-          biome
-          python3Packages.black
-          cbfmt
-          deno
-          dprint
-          eslint_d
-          fixjson
-          python3Packages.isort
-          oxlint
-          prettier
-          prettierd
-          rustfmt
-          shfmt
-          stylua
-          # tombi not available in nixpkgs
-          xmlformat
-
-          # Debuggers
-          delve
-          vscode-extensions.vadimcn.vscode-lldb
-
-          # CLI Tools
-          lazygit
-          tig
-          gcc
-          gnumake
-          cargo
-          direnv
-          nodejs
-          yarn
-          docker
-          difftastic
-          util-linux
-          coreutils
-          curl
-          bash
-          findutils
-          zoxide
-        ];
-    in
-    {
-      homeManagerModules.default =
-        {
-          config,
-          lib,
-          pkgs,
-          ...
-        }:
-        {
-          config.programs.neovim.enable = true;
-          config.programs.neovim.extraPackages = neovimTools pkgs;
-          config.xdg.configFile."nvim".source = ./.;
+      homeManagerModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: {
+        config = {
+          programs.neovim = {
+            enable = true;
+            extraPackages = neovimTools pkgs;
+            plugins = [
+              pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+            ];
+          };
+          xdg.configFile."nvim".source = ./.;
         };
+      };
     }
     // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         devShells.default = pkgs.mkShell {
           packages = neovimTools pkgs;
           shellHook = ''
--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -1,64 +1,79 @@
+local function safe_require(module_name, callback)
+	local status, module = pcall(require, module_name)
+	if status and callback then
+		callback(module)
+	elseif not status then
+		vim.api.nvim_echo(
+			{ { "Module " .. module_name .. " not found", "WarningMsg" } },
+			true,
+			{}
+		)
+	end
+	return status, module
+end
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
-	require("nvim-treesitter.configs").setup {
-		-- auto_install = true,
-		highlight = {
-			enable = true,
-			disable = function(_, bufnr)
-				return vim.api.nvim_buf_line_count(bufnr) > 5000
-			end,
-			additional_vim_regex_highlighting = {
-				"org",
-				"diff",
-				"gitcommit",
+	safe_require("nvim-treesitter.configs", function(configs)
+		configs.setup {
+			-- auto_install = true,
+			highlight = {
+				enable = true,
+				disable = function(_, bufnr)
+					return vim.api.nvim_buf_line_count(bufnr) > 5000
+				end,
+				additional_vim_regex_highlighting = {
+					"org",
+					"diff",
+					"gitcommit",
+				},
 			},
-		},
-	}
+		}
+	end)
 
-	local parser_config =
-		require("nvim-treesitter.parsers").get_parser_configs()
+	safe_require("nvim-treesitter.parsers", function(parsers)
+		local parser_config = parsers.get_parser_configs()
 
-	parser_config.kanshi = {
-		install_info = {
-			url = "https://github.com/marcelarie/tree-sitter-kanshi",
-			-- url = "~/clones/own/tree-sitter-kanshi",
-			files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-			-- optional entries:
-			branch = "main", -- default branch in case of git repo if different from master
-			generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-			requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-		},
-		filetype = "kanshi", -- if filetype does not match the parser name
-	}
+		parser_config.kanshi = {
+			install_info = {
+				url = "https://github.com/marcelarie/tree-sitter-kanshi",
+				-- url = "~/clones/own/tree-sitter-kanshi",
+				files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+				-- optional entries:
+				branch = "main", -- default branch in case of git repo if different from master
+				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+			},
+			filetype = "kanshi", -- if filetype does not match the parser name
+		}
 
-	parser_config.zig = {
-		install_info = {
-			url = "~/clones/forks/tree-sitter-zig",
-			files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-			-- optional entries:
-			branch = "main", -- default branch in case of git repo if different from master
-			generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-			requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-		},
-		filetype = "zig", -- if filetype does not match the parser name
-	}
+		parser_config.zig = {
+			install_info = {
+				url = "~/clones/forks/tree-sitter-zig",
+				files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+				-- optional entries:
+				branch = "main", -- default branch in case of git repo if different from master
+				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+			},
+			filetype = "zig", -- if filetype does not match the parser name
+		}
+		-- require("nvim-treesitter.parsers").get_parser_configs().kanshi = {
+		-- 	install_info = {
+		-- 		url = "~/clones/own/tree-sitter-kanshi/",
+		-- 		files = { "src/parser.c" },
+		-- 	},
+		-- 	filetype = "kanshi", -- Filetype associated with this grammar
+		-- }
 
-	-- require("nvim-treesitter.parsers").get_parser_configs().kanshi = {
-	-- 	install_info = {
-	-- 		url = "~/clones/own/tree-sitter-kanshi/",
-	-- 		files = { "src/parser.c" },
-	-- 	},
-	-- 	filetype = "kanshi", -- Filetype associated with this grammar
-	-- }
-
-	-- vim.keymap.set(
-	-- 	"n",
-	-- 	"<leader>ts",
-	-- 	":TSPlaygroundToggle<CR>",
-	-- 	{ desc = "Toggle Treesitter playground" }
-	-- )
+		-- vim.keymap.set(
+		-- 	"n",
+		-- 	"<leader>ts",
+		-- 	":TSPlaygroundToggle<CR>",
+		-- 	{ desc = "Toggle Treesitter playground" }
+		-- )
+	end)
 end, 0)
 
 -- vim: ts=2 sts=2 sw=2 et
